c6b26eda1946711aff17ca700d06e370f02aa9c0
Elec332 <arnout1998@gmail.com>
2014-09-16 16:22:01 +0200

    TEst

--- Recources/Scripts/loadmenu.js
+++ Recources/Scripts/loadmenu.js
@@ -1,6 +1,6 @@
 <script src="./Recources/Scripts/jquery/jquery.js"></script> 
     <script> 
     $(function(){
-      $("#includedContent").load("./Recources/Stylesheet/menu.html"); 
+      $("#includedContent").load("../Stylesheet/menu.html"); 
     });
     </script> 
