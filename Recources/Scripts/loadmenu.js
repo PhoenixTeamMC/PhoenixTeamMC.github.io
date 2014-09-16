c9d32876c83dace17deca954d23a9b1efeae7047
Elec332 <arnout1998@gmail.com>
2014-09-16 16:26:49 +0200

    External load?

--- Recources/Scripts/loadmenu.js
+++ Recources/Scripts/loadmenu.js
@@ -1,6 +1,6 @@
 <script src="./Recources/Scripts/jquery/jquery.js"></script> 
     <script> 
     $(function(){
-      $("#includedContent").load("../Stylesheet/menu.html"); 
+      $("#includedContent").load("./Recources/Stylesheet/menu.html"); 
     });
     </script> 
