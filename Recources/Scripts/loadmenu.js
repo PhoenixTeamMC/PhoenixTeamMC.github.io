3b9f1fd977b1839b4ce0dd752eba02ff6c13fc3f
Elec332 <arnout1998@gmail.com>
2014-09-16 16:16:38 +0200

    Code cleanup

---
+++ Recources/Scripts/loadmenu.js
@@ -0,0 +1,6 @@
+<script src="./Recources/Scripts/jquery/jquery.js"></script> 
+    <script> 
+    $(function(){
+      $("#includedContent").load("./Recources/Stylesheet/menu.html"); 
+    });
+    </script> 
